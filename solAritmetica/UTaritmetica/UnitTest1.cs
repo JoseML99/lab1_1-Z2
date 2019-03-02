4d3752c8a74d8851d1d9d31b669bef54084e800c
dieguitopiyin <dmauricio.gs@gmail.com>
2019-03-02 13:43:40 -0400

    hfh

--- solAritmetica/UTaritmetica/UnitTest1.cs
+++ solAritmetica/UTaritmetica/UnitTest1.cs
@@ -9,6 +9,20 @@ namespace UTaritmetica
         [TestMethod]
         public void TestMethod1()
         {
+            /// arrange
+            const int numa = 5;
+            const int numb = 3;
+            const int numc = 8;
+
+            /// act
+            using (SRVaritmetica.WSaritmeticaClient cliente = new SRVaritmetica.WSaritmeticaClient())
+            {
+                var obj = cliente.sumar(numa, numb);
+
+                /// assert
+                Assert.AreEqual(numc, obj.resultado);
+            }
+
         }
     }
 }
